8e0318fb4abc558158542329774f6c556fce18b1
doodream <macbook@Macbookui-MacBookPro.local>
2020-12-08 20:25:14 +0900

    what the fucking error

--- src/containers/home/Section/SlideSection.js
+++ src/containers/home/Section/SlideSection.js
@@ -4,22 +4,22 @@ import {makeStyles} from "@material-ui/core/styles";
 import styles from './SlideSection.style.js';
 
 
-const useStyles = makeStyles(styles);
-const images = [
+const useStyles=makeStyles(styles);
+const images=[
     'https://i.pinimg.com/564x/12/1d/0a/121d0a67f61b86e5d3094e099c5de939.jpg',
     'https://t1.daumcdn.net/cfile/tistory/131A95204B36CDC76B',
     'https://t1.daumcdn.net/cfile/tistory/196457224B36CDDE67?original',
 ];
 
 export default function SlideSection(){
-    const classes = useStyles();
-    const [count, setCount] = useState(0);
+    const classes=useStyles();
+    const [count, setCount]=useState(0);
     useEffect(()=>{
         setTimeout(()=>{
-            if(count === images.length - 1) {
+            if(count===images.length-1) {
                 setCount(0);
             }else{
-                setCount(count + 1);
+                setCount(count+1 );
             }
         },3000);
     })
@@ -27,7 +27,7 @@ export default function SlideSection(){
     return (
         <Box>
             <Box className={classes.imageSection}>
-                <img className='backImage' src = {images[count]} alt = '배경이미지'></img>
+                <img className='backImage' src={images[count]} alt='배경이미지'></img>
             </Box>
             <Box className={classes.title}>
                 <Typography variant='h3'>세상의 모든 자유여행 상품 비교</Typography>
