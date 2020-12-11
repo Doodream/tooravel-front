f2cc9d1d37c0a4d75e31ec94896f8bb43b56b365
doodream <macbook@Macbookui-MacBookPro.local>
2020-12-11 23:14:58 +0900

    Add validate email, password, reEnterPassword

--- src/domains/account/Signup.js
+++ src/domains/account/Signup.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import {Box, Button, Divider, TextField, Typography} from '@material-ui/core';
 import {makeStyles} from '@material-ui/core/styles';
 import styles from './Login.style.js'
@@ -13,13 +13,67 @@ export default function Signup({ history }){
     const emailValue = useRef(null);
     const passwordFormCheck = useRef(null);
     const passwordValue = useRef(null);
+    const reEnterPasswordFormCheck = useRef(null);
+
+    const [emailEntered, setEmailEntered] = useState('');
+    const [isEmailValid, setIsEmailValid] = useState(false);
+    const [passwordEntered, setPasswordEntered] = useState('');
+    const [isPasswordValid, setIsPasswordValid] = useState(false);
+    const [isReEnterPasswordValid, setIsReEnterPasswordValid] = useState(false);
 
     // 이메일 형식 검사, 비밀번호 형식 검사
+    const validateEmail = (emailEntered) => {
+        const emailRegex = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+        if(emailEntered.match(emailRegex)){
+            setEmailEntered(emailEntered);
+            setIsEmailValid(true);
+        }else{
+            setEmailEntered(emailEntered);
+            setIsEmailValid(false);
+        }
+    }
+    const validatePassword = (passwordEntered) =>{
+        // 특수문자 / 문자 / 숫자포함 8 ~ 15자리
+        const passwordRegex = /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
+
+        if(passwordEntered.match(passwordRegex)){
+            setPasswordEntered(passwordEntered);
+            setIsPasswordValid(true);
+        }else{
+            setPasswordEntered(passwordEntered);
+            setIsPasswordValid(false);
+        }
+    }
+    const validateReEnterPassword = (passwordReEntered) =>{
+        if(passwordReEntered === passwordEntered){
+            setIsReEnterPasswordValid(true);
+        }else{
+            setIsReEnterPasswordValid(false);
+        }
+    }
+    // 이메일 형식 검사, 비밀번호 형식 검사
     const formCheck = () => {
-        if(emailValue.current.value === '');
+        if(!isEmailValid){
             emailFormCheck.current.style.color = 'red';
-        if(passwordValue.current.value === '');
+        }
+        else{
+            emailFormCheck.current.style.color = 'white';
+        }
+
+        if(!isPasswordValid){
             passwordFormCheck.current.style.color = 'red';
+        }
+        else{
+            passwordFormCheck.current.style.color = 'white';
+        }
+
+        if(!isReEnterPasswordValid){
+            reEnterPasswordFormCheck.current.style.color = 'red';
+        }else{
+            reEnterPasswordFormCheck.current.style.color = 'white';
+        }
+
     }
 
     return (
@@ -32,22 +86,25 @@ export default function Signup({ history }){
                     <Divider  className={classes.loginDivider}></Divider>
                     <TextField 
                         className={classes.loginInput} 
-                        ref={emailValue} 
-                        label='Email' 
-                        variant='outlined' type='email' 
-                        autoComplete='email'/>
+                        onChange={(e)=>{validateEmail(e.target.value)}}
+                        label='Email : abc@gmail.com' 
+                        autoComplete='email'
+                        variant='outlined' type='email'/>
                     <Typography className={classes.loginInputErrmsg} ref={emailFormCheck}> 이메일을 형식에 맞게 입력해주세요 </Typography>
                     <TextField className={classes.loginInput, classes.loginPassword} 
                         ref={passwordValue} 
-                        label='Password' 
+                        label='Password : 숫자 문자 특수 문자 8 ~ 15 자리 이상'
+                        onChange={(e)=>{validatePassword(e.target.value)}}
                         variant='outlined' 
                         type='password'/>
+                    <Typography className={classes.loginInputErrmsg} ref={passwordFormCheck}> 비밀번호를 형식에 맞게 입력해주세요 </Typography>
                     <TextField className={classes.loginInput, classes.loginPassword} 
                         ref={passwordValue} 
-                        label='Re-enter Password' 
+                        label='Re-enter Password'
+                        onChange={(e)=>{validateReEnterPassword(e.target.value)}} 
                         variant='outlined' 
                         type='password'/>
-                    <Typography className={classes.loginInputErrmsg} ref={passwordFormCheck}> 비밀번호를 입력해주세요 </Typography>
+                    <Typography className={classes.loginInputErrmsg} ref={reEnterPasswordFormCheck}> 같은 비밀번호를 입력해주세요 </Typography>
                     <Button 
                         className={classes.loginSummitButton} 
                         onClick={formCheck}
