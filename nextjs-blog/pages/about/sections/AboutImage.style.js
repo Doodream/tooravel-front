4726ef8d2f62214bf47fa2c7ef385a87aa5761cc
doodream <macbook@Macbookui-MacBookPro.local>
2021-01-13 23:37:04 +0900

    Add about/intro 경로 페이지 추가

---
+++ nextjs-blog/pages/about/sections/AboutImage.style.js
@@ -0,0 +1,14 @@
+const AboutImageStyle = {
+    imageSection: {
+        paddingTop: 77.25,
+        position: 'relative',
+        display: 'flex',
+        width: '100%',
+        height: '567.25px',
+        '& img': {
+            width: '100%',
+            height: '100%',
+        }
+    },
+}
+export default AboutImageStyle;
