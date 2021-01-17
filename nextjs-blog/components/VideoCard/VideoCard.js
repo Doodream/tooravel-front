119b9c72fcfb73530a03929bec777cdc111b88bb
doodream <macbook@Macbookui-MacBookPro.local>
2021-01-17 23:49:01 +0900

    Verify : VideoCard.js : 메인 컴포넌트에서 videoInfo를 받아와서 렌더링만 하는 컴포넌트로 바꿈

--- nextjs-blog/components/VideoCard/VideoCard.js
+++ nextjs-blog/components/VideoCard/VideoCard.js
@@ -1,30 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Box, Typography, Button, Link, Card, CardContent, CardActions, Modal } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Box, Typography, Button, Card, CardContent, CardActions, Modal } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { makeStyles } from "@material-ui/core/styles";
 import styles from './VideoCard.style.js';
-import axios from 'axios';
 
 const useStyles = makeStyles(styles);
-const KEY = 'AIzaSyABIHpDoCRz-SxK7mCI54mqqSKvF9wvP4Y';
-
-export default function VideoCard({ videoID }) {
+export default function VideoCard({ videoInfo }) {
     const classes = useStyles();
-    const [viewCount, setViewCount] = useState(0);
-    const [publishedAt, setPublishedAt] = useState('');
-    const [channelTitle, setChannelTitle] = useState('');
-    const [title, setTitle] = useState('');
-    const [thumbnail, setThumbnail] = useState('');
     const [open, setOpen] = useState('');
-
-    const getVideoInfo = async (videoID) => {
-        const { data: { items } } = await axios.get(`https://www.googleapis.com/youtube/v3/videos?id=${videoID}&key=${KEY}&part=snippet,statistics&fields=items(id,snippet(channelId,channelTitle,title,publishedAt,thumbnails(high)),statistics(viewCount))`);
-        setViewCount(items[0].statistics.viewCount);
-        setPublishedAt(items[0].snippet.publishedAt.substring(0, 10));
-        setChannelTitle(items[0].snippet.channelTitle);
-        setTitle(items[0].snippet.title);
-        setThumbnail(items[0].snippet.thumbnails.high.url);
-    }
 
     const handleOpen = () => {
         setOpen(true);
@@ -33,16 +16,11 @@ export default function VideoCard({ videoID }) {
         setOpen(false);
     }
 
-    useEffect(() => {
-        getVideoInfo(videoID)
-    }, [])
-    // 첫 렌더링에만 호출
-
-    let viewRecount = String(viewCount);
-    if (viewCount > 10000) {
-        viewRecount = String(parseInt(viewCount / 10000)) + '만';
-    } else if (viewCount > 1000) {
-        viewRecount = String(parseInt(viewCount / 1000)) + '천';
+    let viewRecount = String(videoInfo.viewCount);
+    if (videoInfo.viewCount > 10000) {
+        viewRecount = String(parseInt(videoInfo.viewCount / 10000)) + '만';
+    } else if (videoInfo.viewCount > 1000) {
+        viewRecount = String(parseInt(videoInfo.viewCount / 1000)) + '천';
     }
 
     return (
@@ -50,20 +28,20 @@ export default function VideoCard({ videoID }) {
             <Modal className={classes.videoModal}
                 open={open}
                 onClose={handleClose}>
-                <iframe width="1000" height="600" src={`https://www.youtube.com/embed/${videoID}`} frameborder="0" allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='true'></iframe>
+                <iframe width="1000" height="600" src={`https://www.youtube.com/embed/${videoInfo.videoId}`} frameborder="0" allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='true'></iframe>
             </Modal>
             <Button className={classes.videoCard} onClick={() => handleOpen()}>
                 <Card className={classes.videoCardSection}>
                     <CardActions classeName={classes.videoCardImage}>
-                        <img className={classes.videoCardThumbnail} src={thumbnail} alt='썸네일'></img>
+                        <img className={classes.videoCardThumbnail} src={videoInfo.thumbnail} alt='썸네일'></img>
                         {/* <Box className={classes.videoCardLogo}>
                             <img src='https://www.tooravel.net/static/common/img/icon_ytb.png' alt='youtube로고'></img>
                         </Box> */}
                     </CardActions>
                     <CardContent className={classes.videoCardContent}>
-                        <Typography >조회수 {viewRecount}회 · {publishedAt}</Typography>
-                        <Typography >by {channelTitle}</Typography>
-                        <Typography >{title}</Typography>
+                        <Typography >조회수 {viewRecount}회 · {videoInfo.publishedAt}</Typography>
+                        <Typography >by {videoInfo.channelTitle}</Typography>
+                        <Typography >{videoInfo.title}</Typography>
                     </CardContent>
                 </Card>
             </Button>
@@ -72,9 +50,5 @@ export default function VideoCard({ videoID }) {
 }
 
 VideoCard.propTypes = {
-    viewCount: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    channelTitle: PropTypes.string.isRequired,
-    publishedAt: PropTypes.string.isRequired,
-    thumbnail: PropTypes.string.isRequired,
+    videoInfo: PropTypes.object.isRequired,
 };
