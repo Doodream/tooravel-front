1c75656d875516d596832f98aa29161686316540
doodream <macbook@Macbookui-MacBookPro.local>
2021-02-08 21:15:21 +0900

    Verify : product Page nextjs Link 적용을 하지 않은 것을 수정

--- pages/home/section/RentBanner.js
+++ pages/home/section/RentBanner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { Box, Typography, Button } from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles";
 import styles from './RentBanner.style.js';
@@ -17,9 +18,13 @@ export default function SlideSection() {
                 <Typography variant='h5'>하루 대여료 최저가 5900원</Typography>
                 <Typography variant='h2'>고프로 빌려서 여행가자!</Typography>
             </Box>
-            <Button href='/product' className={classes.rentBannerButton}>
-                <Typography>고프로 대여하기</Typography>
-            </Button>
+            <Link href='/product'>
+                <a>
+                    <Button className={classes.rentBannerButton}>
+                        <Typography>고프로 대여하기</Typography>
+                    </Button>
+                </a>
+            </Link>
         </Box>
     )
 }
